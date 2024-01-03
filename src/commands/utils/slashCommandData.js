010a4e300c6580537fafa891bdf374fbbaaeb185
kuroma6666 <kuroma.creator@gmail.com>
2024-01-03 23:56:44 +0900

    add: スラッシュコマンド用の使用方法データ構造ファイルを追加

---
+++ src/commands/utils/slashCommandData.js
@@ -0,0 +1,12 @@
+const command_usage = [
+    {
+        name: "omikuji",
+        description: "おみくじをします",
+    },
+    {
+        name: "ping",
+        description: "ping値を表示します",
+    },
+];
+
+exports.command_usage = command_usage;
